bd81d488a310db896669b46eab58d80afb2fae99
Sh3z <t.sherwood@me.com>
2014-07-19 00:49:35 +0100

    Tidy-up

--- SZEnumerate/SZEnumerate.h
+++ SZEnumerate/SZEnumerate.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SZEnumerateVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SZEnumerate/PublicHeader.h>
 
-
+#import "SZEnumerateExtensions.h"
