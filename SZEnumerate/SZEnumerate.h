6027ca69d59627788b3845edc2e5e5ace6b4027e
Sh3z <t.sherwood@me.com>
2014-07-18 18:54:03 +0100

    Initial Commit

---
+++ SZEnumerate/SZEnumerate.h
@@ -0,0 +1,19 @@
+//
+//  SZEnumerate.h
+//  SZEnumerate
+//
+//  Created by Thomas Sherwood on 18/07/2014.
+//  Copyright (c) 2014 Thomas Sherwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SZEnumerate.
+FOUNDATION_EXPORT double SZEnumerateVersionNumber;
+
+//! Project version string for SZEnumerate.
+FOUNDATION_EXPORT const unsigned char SZEnumerateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SZEnumerate/PublicHeader.h>
+
+
